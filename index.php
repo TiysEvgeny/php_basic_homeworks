70adc28e5dba811a5ff1e2a86a3d4999facefe0a
TiysEvgeny <tiys@ngs.ru>
2019-01-18 16:44:08 +0700

    Добавил решение пункта 1

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	echo "<h1>ДЗ2</h1>";
+	echo "<h2>Задание 1</h2>";
+	$a = -2;
+	$b = 3;
+	if($a>=0 && $b>=0){
+		echo $a-$b;
+	}else if($a <0 && $b <0){
+		echo $a*$b;
+	}else{
+		echo $a+$b;
+	}
+?>
